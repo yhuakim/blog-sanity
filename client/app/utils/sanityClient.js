4decdc9659e04569e4e08fd97f12e49312f0f612
ARINZE JOACHIM OJILI <47617723+yhuakim@users.noreply.github.com>
2023-07-31 13:29:25 +0100

    removed: client directory

--- client/app/utils/sanityClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, //Replace this with your project ID
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: "2023-07-24",
-    useCdn: "true",
-})
--- client/app/utils/sanityImageUrl.js
+++
@@ -1,8 +0,0 @@
-import ImageUrlBuilder from "@sanity/image-url";
-import { client } from "./sanityClient";
-
-const builder = ImageUrlBuilder(client);
-
-export function urlFor(source) {
-    return builder.image(source);
-}
